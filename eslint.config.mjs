be5bac0ccd5528f09d319986f678bfdb06ebd8d3
Yuen Wee Kin Edwin <edwin.edwin313@outlook.com>
2025-06-16 17:02:12 +0800

    Eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,63 @@
+// ESLint core and configuration.
+import {defineConfig} from "eslint/config";
+
+// Built-in ESLint language plugins.
 import js from "@eslint/js";
-import globals from "globals";
 import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
-import { defineConfig } from "eslint/config";
 
+// External plugins
+import security from "eslint-plugin-security";
+import securityNode from "eslint-plugin-security-node";
+import noUnsanitized from "eslint-plugin-no-unsanitized"
+import html from "eslint-plugin-html"
+
+// Global variables (browser, node, etc.)
+import globals from "globals";
+
+const commonParserOptions = {
+    ecmaVersion: 2021,
+    sourceType: "module",
+}
+const commonGlobals = globals.browser
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+    {
+        files: ["**/*.{js,mjs,cjs}"],
+        plugins: {
+            js,
+            security,
+            "security-node": securityNode,
+            "no-unsanitized": noUnsanitized,
+        },
+        extends: [
+            "plugin:security-node/recommended",
+            "plugin:no-unsanitized/recommended"
+        ],
+        rules: {
+            ...js.configs.recommended.rules,
+            ...security.configs.recommended.rules,
+
+            "security/detect-eval-with-expression": "error",
+            "security-node/detect-crlf": "warn",
+        },
+        languageOptions: {
+            parserOptions: commonParserOptions,
+            globals: commonGlobals,
+        },
+    },
+    {files: ["**/*.json"], plugins: {json}, language: "json/json", extends: ["json/recommended"]},
+    {files: ["**/*.md"], plugins: {markdown}, language: "markdown/gfm", extends: ["markdown/recommended"]},
+    {files: ["**/*.css"], plugins: {css}, language: "css/css", extends: ["css/recommended"]},
+    {
+        files: ["**/*.html"],
+        plugins: {html},
+        languageOptions: {
+            parserOptions: commonParserOptions,
+            globals: commonGlobals,
+        },
+        settings: {},
+        rules: {},
+    }
 ]);
